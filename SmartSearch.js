87ae229e005d8991ce91329f6fba13c07efa7bb7
nayeon-factual <nayeon@factual.com>
2014-07-29 18:18:59 -0700

    working on converting category labels to IDs

--- SmartSearch.js
+++ SmartSearch.js
@@ -1,10 +1,10 @@
 
 var facetable = ["country", "region", "state", "province", "locality", "town", "city", "zipcode", "zip", "postcode", "postalcode", "category", "chain", "chain_name"];
-var filtersHistory = {country:0, region:0, locality:0, post_town:0, category:0, chain_name:0};
+var filtersHistory = {country:0, region:0, locality:0, post_town:0, category_ids:0, chain_name:0};
 var filtersCount = 0;
 var qHistory = [];
 var URL = "http://www.factual.com/data/t/places#";
-retrieve();
+getCategoryID();
 
 function ClearFields() {
      document.getElementById("searchInput").value = "";
@@ -25,6 +25,8 @@ function categorize(filterName) {
         filterName = "locality";
     }else if(filterName == "chain") {
         filterName = "chain_name";
+    }else if(filterName == "category") {
+        filterName = "category_ids";
     }else if(filterName == "zipcode" || filterName == "zip" || filterName == "postcode" || filterName == "postalcode"){
         filterName = "post_town";   
     }
@@ -52,10 +54,38 @@ function hasFilter(input) {
     }
 }
 
+function getCategoryID(){
+    $.getJSON('places-categories.json', function(data) {
+        
+    })
+}
+
+function generateURL(fName, filterValue) {
+    if (filtersCount==0) {
+        firstInput = formatNewFilterInput(fName, filterValue);
+        //check if there is already a keyword filter  
+        if(qHistory.length==0){
+            URL = URL+'filters={"$and":['+firstInput+']}';
+        }else{
+            URL = URL+'&filters={"$and":['+firstInput+']}';
+        }
+
+    } else if (filtersHistory[fName]==0) {
+        newFilterInput = formatNewFilterInput(fName, filterValue);
+        URL = URL.slice(0, URL.indexOf('filters={"$and":[')+17)+newFilterInput+','+URL.slice(URL.indexOf('filters={"$and":[')+17); 
+
+    } else if (filtersHistory[fName]>0) {
+        filterValue = filterValue.split(' ').join('+');
+        URL = URL.split('"'+fName+'":{"$eq"').join('"'+fName+'":{"$in"');
+        var firstIndex = URL.indexOf(fName+'":{"$in":')+fName.length+9;
+        var closingBraceIndex = URL.slice(firstIndex).indexOf("}}")
+        URL = URL.slice(0, firstIndex) + '["' + filterValue + '", ' + URL.slice(firstIndex).slice(0,closingBraceIndex) +']'+ URL.slice(firstIndex).slice(closingBraceIndex);
+    }
+}
+
 function goToData() {
     var searchInput = document.getElementById("searchInput").value;
     $("#history").append('<div class="filterbox">'+searchInput+'</div>');
-    //= document.getElementById("history").innerHTML+searchInput+"<br/>"; 
     console.log(hasFilter(searchInput));
         //if search contains a filter, then separate filter and search values
         if(hasFilter(searchInput)){
@@ -63,33 +93,17 @@ function goToData() {
             var filterValue = searchInput.substring(searchInput.indexOf(":")+1);
             //check if filter is facetable
             if(-1 < facetable.indexOf(filterName)){
-                fName = categorize(filterName);
                 //**TODO** regexes check
-
-                if (filtersCount==0) {
-                    firstInput = formatNewFilterInput(fName, filterValue);
-                    //check if there is already a keyword filter  
-                    if(qHistory.length==0){
-                        URL = URL+'filters={"$and":['+firstInput+']}';
-                    }else{
-                        URL = URL+'&filters={"$and":['+firstInput+']}';
-                    }
-
-                } else if (filtersHistory[fName]==0) {
-                    newFilterInput = formatNewFilterInput(fName, filterValue);
-                    URL = URL.slice(0, URL.indexOf('filters={"$and":[')+17)+newFilterInput+','+URL.slice(URL.indexOf('filters={"$and":[')+17); 
-
-                } else if (filtersHistory[fName]>0) {
-                    filterValue = filterValue.split(' ').join('+');
-                    URL = URL.split('"'+fName+'":{"$eq"').join('"'+fName+'":{"$in"');
-                    var firstIndex = URL.indexOf(fName+'":{"$in":')+fName.length+9;
-                    var closingBraceIndex = URL.slice(firstIndex).indexOf("}}")
-                    URL = URL.slice(0, firstIndex) + '["' + filterValue + '", ' + URL.slice(firstIndex).slice(0,closingBraceIndex) +']'+ URL.slice(firstIndex).slice(closingBraceIndex);
+                if(filterName == "category"){
+                    fName = categorize(filterName);
+                    var filterID = getCategoryID(filterValue);
+                    generateURL(fName, filterID);
+                }else{
+                    fName = categorize(filterName);
+                    generateURL(fName, filterValue);
                 }
-                
                 filtersHistory[fName]+=1;
                 filtersCount += 1;
-//                window.open(URL);
                 
             }else{
                 //**TODO** "Did you mean:"
@@ -107,7 +121,6 @@ function goToData() {
                 URL = URL.slice(0, URL.indexOf("q=")+2)+qInput+','+URL.slice(URL.indexOf("q=")+2);
             }
             qHistory.push(qInput);
-//            window.open(URL);
         }
     ClearFields();
 }
@@ -115,7 +128,3 @@ function goToData() {
 function openURL() {
     window.open(URL);
 }
-
-//function retrieve() {
-//    console.log($.get('http://api.v3.factual.com/t/restaurants-us?filters={"name":{"$bw":"star"}}&include_count=true'));
-//}
