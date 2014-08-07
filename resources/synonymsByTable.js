fc2e220ab8e3e2c0b411554ae0b0c22df5e63a9c
nayeon-factual <nayeon@factual.com>
2014-08-07 14:27:28 -0700

    adding synonyms table

---
+++ resources/synonymsByTable.js
@@ -0,0 +1,13 @@
+var synonyms = {
+"places":{
+    'locality':['town','city'],
+    'region':['state','province'],
+    'country':[],
+    'category_ids':['category','categories','category label', 'category name'],
+    'chain_name':['chain','chains','chain name'],
+    'post_town':['post town'],
+    'admin_region':['admin region']},
+"restaurants-us":{},
+"restaurants-gb":{},
+"healthcare-providers-us":{}
+}
